5d5711de2fef300ab0e3977b409430f944d2665f
Cheng-I Lai <clai24@sls-sm-8.csail.mit.edu>
2020-11-18 17:33:06 -0500

    fine-tuning for asr

--- examples/wav2vec/run.sh
+++ examples/wav2vec/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash 
 
 stage=$1
+finetune=$2
 ngpu=1
+
 
 if [ $stage -eq 0 ]; then 
     for split in dev-clean dev-other train-clean-100 test-clean test-other train-clean-360 train-960; do 
@@ -62,27 +64,63 @@ if [ $stage -eq 3 ]; then
 fi
 
 if [ $stage -eq 4 ]; then
-	## train from scratch commands
     train_subset=train-960
     valid_subset=dev-clean
     expdir=exp/${train_subset}/distill_ce-att_head_6-embed_dim_480-ffn_embed_dim_1920-layer_3-latent_vars_160
     datadir=data/${train_subset}
     mkdir -p $expdir
 
-	python /data/sls/temp/clai24/knowledge-transfer/fairseq/train.py --distributed-world-size ${ngpu} --distributed-port 0 $datadir --save-dir $expdir \
-        --train-subset $train_subset --valid-subset $valid_subset \
-		--num-workers 8 --task audio_pretraining --criterion wav2vec_kd --arch wav2vec2 \
-		--log-keys '["prob_perplexity","code_perplexity","temp"]' --quantize-targets --extractor-mode default \
-		--conv-feature-layers '[(512, 10, 5)] + [(512, 3, 2)] * 4 + [(512,2,2)] * 2' --final-dim 256 \
-		--latent-groups 2 --latent-temp '(2,0.5,0.999995)' --infonce --optimizer adam \
-		--adam-betas '(0.9,0.98)' --adam-eps 1e-06 --lr-scheduler polynomial_decay --total-num-update 400000 \
-		--lr 0.0005 --warmup-updates 32000 --mask-length 10 --mask-prob 0.65 --mask-selection static --mask-other 0 \
-		--encoder-layerdrop 0.05 --dropout-input 0.1 --dropout-features 0.1 --feature-grad-mult 0.1 \
-		--loss-weights '[0.1, 10]' --conv-pos 128 --conv-pos-groups 16 --num-negatives 100 --cross-sample-negatives 0 \
-		--max-sample-size 250000 --min-sample-size 32000 --dropout 0.1 --attention-dropout 0.1 --weight-decay 0.01 \
-		--max-tokens 1400000 --max-update 400000 --skip-invalid-size-inputs-valid-test --log-format tqdm --ddp-backend no_c10d \
-        --encoder-attention-heads 6 --encoder-embed-dim 480 --encoder-ffn-embed-dim 1920 --encoder-layers 3 --latent-vars 160 \
-        --distill-with-ce 2>&1 | tee $expdir/train.log
+    finetune_train_subset=train-clean-100
+    finetune_valid_subset=dev-clean
+    finetune_datadir=data/${finetune_train_subset}
+    finetune_expdir=${expdir}/fine_tune-${finetune_train_subset}
+    mkdir -p $finetune_expdir
+
+    if $finetune; then 
+        echo 'fine-tuning'
+        ## fine-tuning commands
+        # hyper-parameter for fine-tuning on 100 hr (Table 6 in https://arxiv.org/pdf/2006.11477.pdf)
+        # validate on the dev-clean subset
+        
+        python /data/sls/scratch/clai24/fairseq/train.py --distributed-world-size ${ngpu} --distributed-port 0 $finetune_datadir --save-dir $finetune_expdir \
+            --post-process letter --train-subset $finetune_train_subset --valid-subset $finetune_valid_subset --no-epoch-checkpoints --best-checkpoint-metric wer --num-workers 8 \
+            --max-update 50000 --sentence-avg --task audio_pretraining --arch wav2vec_ctc --w2v-path /data/sls/temp/clai24/pretrained-models/wav2vec_small.pt \
+            --labels ltr --apply-mask --mask-selection static --mask-other 0 --mask-length 10 --mask-prob 0.5 --layerdrop 0.1 \
+            --mask-channel-selection static --mask-channel-other 0 --mask-channel-length 64 --mask-channel-prob 0.512 --zero-infinity \
+            --feature-grad-mult 0.0 --freeze-finetune-updates 10000 --validate-after-updates 10000 --optimizer adam \
+            --adam-betas '(0.9, 0.98)' --adam-eps 1e-08 --lr 2e-05 --lr-scheduler tri_stage --warmup-steps 5000 --hold-steps 20000 \
+            --decay-steps 25000 --final-lr-scale 0.05 --final-dropout 0.0 --dropout 0.0 --activation-dropout 0.1 --criterion ctc \
+            --attention-dropout 0.0 --max-tokens 1280000 --seed 2337 --log-format tqdm --log-interval=100 --ddp-backend no_c10d \
+            --update-freq $(( 24/$ngpu )) 2>&1 | tee $finetune_expdir/train.log
+
+        #python /data/sls/temp/clai24/knowledge-transfer/fairseq/train_orig.py --distributed-world-size ${ngpu} --distributed-port 0 $finetune_datadir --save-dir $finetune_expdir \
+        #    --post-process letter --train-subset $finetune_train_subset --valid-subset $finetune_valid_subset --no-epoch-checkpoints --best-checkpoint-metric wer --num-workers 8 \
+        #    --max-update 50000 --sentence-avg --task audio_pretraining --arch wav2vec_ctc --w2v-path $expdir/checkpoint_best.pt \
+        #    --labels ltr --apply-mask --mask-selection static --mask-other 0 --mask-length 10 --mask-prob 0.5 --layerdrop 0.1 \
+        #    --mask-channel-selection static --mask-channel-other 0 --mask-channel-length 64 --mask-channel-prob 0.512 --zero-infinity \
+        #    --feature-grad-mult 0.0 --freeze-finetune-updates 10000 --validate-after-updates 10000 --optimizer adam \
+        #    --adam-betas '(0.9, 0.98)' --adam-eps 1e-08 --lr 2e-05 --lr-scheduler tri_stage --warmup-steps 5000 --hold-steps 20000 \
+        #    --decay-steps 25000 --final-lr-scale 0.05 --final-dropout 0.0 --dropout 0.0 --activation-dropout 0.1 --criterion ctc \
+        #    --attention-dropout 0.0 --max-tokens 1280000 --seed 2337 --log-format tqdm --log-interval=100 --ddp-backend no_c10d \
+        #    --update-freq $(( 24/$ngpu )) 2>&1 | tee $finetune_expdir/train.log
+    else
+	    ## train from scratch commands
+        echo 'pretraining'
+        python /data/sls/temp/clai24/knowledge-transfer/fairseq/train.py --distributed-world-size ${ngpu} --distributed-port 0 $datadir --save-dir $expdir \
+            --train-subset $train_subset --valid-subset $valid_subset \
+            --num-workers 8 --task audio_pretraining --criterion wav2vec_kd --arch wav2vec2 \
+            --log-keys '["prob_perplexity","code_perplexity","temp"]' --quantize-targets --extractor-mode default \
+            --conv-feature-layers '[(512, 10, 5)] + [(512, 3, 2)] * 4 + [(512,2,2)] * 2' --final-dim 256 \
+            --latent-groups 2 --latent-temp '(2,0.5,0.999995)' --infonce --optimizer adam \
+            --adam-betas '(0.9,0.98)' --adam-eps 1e-06 --lr-scheduler polynomial_decay --total-num-update 400000 \
+            --lr 0.0005 --warmup-updates 32000 --mask-length 10 --mask-prob 0.65 --mask-selection static --mask-other 0 \
+            --encoder-layerdrop 0.05 --dropout-input 0.1 --dropout-features 0.1 --feature-grad-mult 0.1 \
+            --loss-weights '[0.1, 10]' --conv-pos 128 --conv-pos-groups 16 --num-negatives 100 --cross-sample-negatives 0 \
+            --max-sample-size 250000 --min-sample-size 32000 --dropout 0.1 --attention-dropout 0.1 --weight-decay 0.01 \
+            --max-tokens 1400000 --max-update 400000 --skip-invalid-size-inputs-valid-test --log-format tqdm --ddp-backend no_c10d \
+            --encoder-attention-heads 6 --encoder-embed-dim 480 --encoder-ffn-embed-dim 1920 --encoder-layers 3 --latent-vars 160 \
+            --distill-with-ce 2>&1 | tee $expdir/train.log
+    fi 
 fi
 
 
@@ -231,6 +269,151 @@ if [ $stage -eq 10 ]; then
         --distill-with-ce-mix 2>&1 | tee $expdir/train.log
 fi
 
+if [ $stage -eq 11 ]; then
+	## train from scratch commands
+    train_subset=train-960
+    valid_subset=dev-clean
+    expdir=exp/${train_subset}/distill_ce_data-att_head_6-embed_dim_480-ffn_embed_dim_1920-layer_6-latent_vars_160
+    datadir=data/${train_subset}
+    mkdir -p $expdir
+
+	python /data/sls/temp/clai24/knowledge-transfer/fairseq/train.py --distributed-world-size ${ngpu} --distributed-port 0 $datadir --save-dir $expdir \
+        --train-subset $train_subset --valid-subset $valid_subset \
+		--num-workers 8 --task audio_pretraining --criterion wav2vec_kd --arch wav2vec2 \
+		--log-keys '["prob_perplexity","code_perplexity","temp"]' --quantize-targets --extractor-mode default \
+		--conv-feature-layers '[(512, 10, 5)] + [(512, 3, 2)] * 4 + [(512,2,2)] * 2' --final-dim 256 \
+		--latent-groups 2 --latent-temp '(2,0.5,0.999995)' --infonce --optimizer adam \
+		--adam-betas '(0.9,0.98)' --adam-eps 1e-06 --lr-scheduler polynomial_decay --total-num-update 400000 \
+		--lr 0.0005 --warmup-updates 32000 --mask-length 10 --mask-prob 0.65 --mask-selection static --mask-other 0 \
+		--encoder-layerdrop 0.05 --dropout-input 0.1 --dropout-features 0.1 --feature-grad-mult 0.1 \
+		--loss-weights '[0.1, 10]' --conv-pos 128 --conv-pos-groups 16 --num-negatives 100 --cross-sample-negatives 0 \
+		--max-sample-size 250000 --min-sample-size 32000 --dropout 0.1 --attention-dropout 0.1 --weight-decay 0.01 \
+		--max-tokens 1400000 --max-update 400000 --skip-invalid-size-inputs-valid-test --log-format tqdm --ddp-backend no_c10d \
+        --encoder-attention-heads 6 --encoder-embed-dim 480 --encoder-ffn-embed-dim 1920 --encoder-layers 6 --latent-vars 160 \
+        --distill-with-ce-data 2>&1 | tee $expdir/train.log
+fi
+
+if [ $stage -eq 12 ]; then
+	## train from scratch commands
+    train_subset=train-960
+    valid_subset=dev-clean
+    expdir=exp/${train_subset}/distill_ce_data-att_head_6-embed_dim_480-ffn_embed_dim_1920-layer_3-latent_vars_160
+    datadir=data/${train_subset}
+    mkdir -p $expdir
+
+	python /data/sls/temp/clai24/knowledge-transfer/fairseq/train.py --distributed-world-size ${ngpu} --distributed-port 0 $datadir --save-dir $expdir \
+        --train-subset $train_subset --valid-subset $valid_subset \
+		--num-workers 8 --task audio_pretraining --criterion wav2vec_kd --arch wav2vec2 \
+		--log-keys '["prob_perplexity","code_perplexity","temp"]' --quantize-targets --extractor-mode default \
+		--conv-feature-layers '[(512, 10, 5)] + [(512, 3, 2)] * 4 + [(512,2,2)] * 2' --final-dim 256 \
+		--latent-groups 2 --latent-temp '(2,0.5,0.999995)' --infonce --optimizer adam \
+		--adam-betas '(0.9,0.98)' --adam-eps 1e-06 --lr-scheduler polynomial_decay --total-num-update 400000 \
+		--lr 0.0005 --warmup-updates 32000 --mask-length 10 --mask-prob 0.65 --mask-selection static --mask-other 0 \
+		--encoder-layerdrop 0.05 --dropout-input 0.1 --dropout-features 0.1 --feature-grad-mult 0.1 \
+		--loss-weights '[0.1, 10]' --conv-pos 128 --conv-pos-groups 16 --num-negatives 100 --cross-sample-negatives 0 \
+		--max-sample-size 250000 --min-sample-size 32000 --dropout 0.1 --attention-dropout 0.1 --weight-decay 0.01 \
+		--max-tokens 1400000 --max-update 400000 --skip-invalid-size-inputs-valid-test --log-format tqdm --ddp-backend no_c10d \
+        --encoder-attention-heads 6 --encoder-embed-dim 480 --encoder-ffn-embed-dim 1920 --encoder-layers 3 --latent-vars 160 \
+        --distill-with-ce-data 2>&1 | tee $expdir/train.log
+fi
+
+
+if [ $stage -eq 13 ]; then
+	## train from scratch commands
+    train_subset=train-960
+    valid_subset=dev-clean
+    expdir=exp/${train_subset}/distill_ce_data-load_extractor-att_head_6-embed_dim_480-ffn_embed_dim_1920-layer_6-latent_vars_160
+    datadir=data/${train_subset}
+    mkdir -p $expdir
+
+	python /data/sls/temp/clai24/knowledge-transfer/fairseq/train.py --distributed-world-size ${ngpu} --distributed-port 0 $datadir --save-dir $expdir \
+        --train-subset $train_subset --valid-subset $valid_subset \
+		--num-workers 8 --task audio_pretraining --criterion wav2vec_kd --arch wav2vec2 \
+		--log-keys '["prob_perplexity","code_perplexity","temp"]' --quantize-targets --extractor-mode default \
+		--conv-feature-layers '[(512, 10, 5)] + [(512, 3, 2)] * 4 + [(512,2,2)] * 2' --final-dim 256 \
+		--latent-groups 2 --latent-temp '(2,0.5,0.999995)' --infonce --optimizer adam \
+		--adam-betas '(0.9,0.98)' --adam-eps 1e-06 --lr-scheduler polynomial_decay --total-num-update 400000 \
+		--lr 0.0005 --warmup-updates 32000 --mask-length 10 --mask-prob 0.65 --mask-selection static --mask-other 0 \
+		--encoder-layerdrop 0.05 --dropout-input 0.1 --dropout-features 0.1 --feature-grad-mult 0.1 \
+		--loss-weights '[0.1, 10]' --conv-pos 128 --conv-pos-groups 16 --num-negatives 100 --cross-sample-negatives 0 \
+		--max-sample-size 250000 --min-sample-size 32000 --dropout 0.1 --attention-dropout 0.1 --weight-decay 0.01 \
+		--max-tokens 1400000 --max-update 400000 --skip-invalid-size-inputs-valid-test --log-format tqdm --ddp-backend no_c10d \
+        --load-extractor --encoder-attention-heads 6 --encoder-embed-dim 480 --encoder-ffn-embed-dim 1920 --encoder-layers 6 --latent-vars 160 \
+        --distill-with-ce-data 2>&1 | tee $expdir/train.log
+fi
+
+if [ $stage -eq 14 ]; then
+	## train from scratch commands
+    train_subset=train-960
+    valid_subset=dev-clean
+    expdir=exp/${train_subset}/distill_ce_data-load_extractor-att_head_6-embed_dim_480-ffn_embed_dim_1920-layer_3-latent_vars_160
+    datadir=data/${train_subset}
+    mkdir -p $expdir
+
+	python /data/sls/temp/clai24/knowledge-transfer/fairseq/train.py --distributed-world-size ${ngpu} --distributed-port 0 $datadir --save-dir $expdir \
+        --train-subset $train_subset --valid-subset $valid_subset \
+		--num-workers 8 --task audio_pretraining --criterion wav2vec_kd --arch wav2vec2 \
+		--log-keys '["prob_perplexity","code_perplexity","temp"]' --quantize-targets --extractor-mode default \
+		--conv-feature-layers '[(512, 10, 5)] + [(512, 3, 2)] * 4 + [(512,2,2)] * 2' --final-dim 256 \
+		--latent-groups 2 --latent-temp '(2,0.5,0.999995)' --infonce --optimizer adam \
+		--adam-betas '(0.9,0.98)' --adam-eps 1e-06 --lr-scheduler polynomial_decay --total-num-update 400000 \
+		--lr 0.0005 --warmup-updates 32000 --mask-length 10 --mask-prob 0.65 --mask-selection static --mask-other 0 \
+		--encoder-layerdrop 0.05 --dropout-input 0.1 --dropout-features 0.1 --feature-grad-mult 0.1 \
+		--loss-weights '[0.1, 10]' --conv-pos 128 --conv-pos-groups 16 --num-negatives 100 --cross-sample-negatives 0 \
+		--max-sample-size 250000 --min-sample-size 32000 --dropout 0.1 --attention-dropout 0.1 --weight-decay 0.01 \
+		--max-tokens 1400000 --max-update 400000 --skip-invalid-size-inputs-valid-test --log-format tqdm --ddp-backend no_c10d \
+        --load-extractor --encoder-attention-heads 6 --encoder-embed-dim 480 --encoder-ffn-embed-dim 1920 --encoder-layers 3 --latent-vars 160 \
+        --distill-with-ce-data 2>&1 | tee $expdir/train.log
+fi
+
+if [ $stage -eq 16 ]; then
+	## train from scratch commands
+    train_subset=train-960
+    valid_subset=dev-clean
+    expdir=exp/${train_subset}/distill_kl-att_head_6-embed_dim_480-ffn_embed_dim_1920-layer_3-latent_vars_160
+    datadir=data/${train_subset}
+    mkdir -p $expdir
+
+	python /data/sls/temp/clai24/knowledge-transfer/fairseq/train.py --distributed-world-size ${ngpu} --distributed-port 0 $datadir --save-dir $expdir \
+        --train-subset $train_subset --valid-subset $valid_subset \
+		--num-workers 8 --task audio_pretraining --criterion wav2vec_kd --arch wav2vec2 \
+		--log-keys '["prob_perplexity","code_perplexity","temp"]' --quantize-targets --extractor-mode default \
+		--conv-feature-layers '[(512, 10, 5)] + [(512, 3, 2)] * 4 + [(512,2,2)] * 2' --final-dim 256 \
+		--latent-groups 2 --latent-temp '(2,0.5,0.999995)' --infonce --optimizer adam \
+		--adam-betas '(0.9,0.98)' --adam-eps 1e-06 --lr-scheduler polynomial_decay --total-num-update 400000 \
+		--lr 0.0005 --warmup-updates 32000 --mask-length 10 --mask-prob 0.65 --mask-selection static --mask-other 0 \
+		--encoder-layerdrop 0.05 --dropout-input 0.1 --dropout-features 0.1 --feature-grad-mult 0.1 \
+		--loss-weights '[0.1, 10]' --conv-pos 128 --conv-pos-groups 16 --num-negatives 100 --cross-sample-negatives 0 \
+		--max-sample-size 250000 --min-sample-size 32000 --dropout 0.1 --attention-dropout 0.1 --weight-decay 0.01 \
+		--max-tokens 1400000 --max-update 400000 --skip-invalid-size-inputs-valid-test --log-format tqdm --ddp-backend no_c10d \
+        --encoder-attention-heads 6 --encoder-embed-dim 480 --encoder-ffn-embed-dim 1920 --encoder-layers 3 --latent-vars 160 \
+        --distill-with-kl 2>&1 | tee $expdir/train.log
+fi
+
+if [ $stage -eq 18 ]; then
+	## train from scratch commands
+    train_subset=train-960
+    valid_subset=dev-clean
+    expdir=exp/${train_subset}/distill_kl-load_extractor-att_head_6-embed_dim_480-ffn_embed_dim_1920-layer_3-latent_vars_160
+    datadir=data/${train_subset}
+    mkdir -p $expdir
+
+	python /data/sls/temp/clai24/knowledge-transfer/fairseq/train.py --distributed-world-size ${ngpu} --distributed-port 0 $datadir --save-dir $expdir \
+        --train-subset $train_subset --valid-subset $valid_subset \
+		--num-workers 8 --task audio_pretraining --criterion wav2vec_kd --arch wav2vec2 \
+		--log-keys '["prob_perplexity","code_perplexity","temp"]' --quantize-targets --extractor-mode default \
+		--conv-feature-layers '[(512, 10, 5)] + [(512, 3, 2)] * 4 + [(512,2,2)] * 2' --final-dim 256 \
+		--latent-groups 2 --latent-temp '(2,0.5,0.999995)' --infonce --optimizer adam \
+		--adam-betas '(0.9,0.98)' --adam-eps 1e-06 --lr-scheduler polynomial_decay --total-num-update 400000 \
+		--lr 0.0005 --warmup-updates 32000 --mask-length 10 --mask-prob 0.65 --mask-selection static --mask-other 0 \
+		--encoder-layerdrop 0.05 --dropout-input 0.1 --dropout-features 0.1 --feature-grad-mult 0.1 \
+		--loss-weights '[0.1, 10]' --conv-pos 128 --conv-pos-groups 16 --num-negatives 100 --cross-sample-negatives 0 \
+		--max-sample-size 250000 --min-sample-size 32000 --dropout 0.1 --attention-dropout 0.1 --weight-decay 0.01 \
+		--max-tokens 1400000 --max-update 400000 --skip-invalid-size-inputs-valid-test --log-format tqdm --ddp-backend no_c10d \
+        --load-extractor --encoder-attention-heads 6 --encoder-embed-dim 480 --encoder-ffn-embed-dim 1920 --encoder-layers 3 --latent-vars 160 \
+        --distill-with-kl 2>&1 | tee $expdir/train.log
+fi
+
 # original 
 if [ $stage -eq -100 ]; then
 	## train from scratch commands
@@ -277,7 +460,7 @@ if [ $stage -eq 100 ]; then
 		--loss-weights '[0.1, 10]' --conv-pos 128 --conv-pos-groups 16 --num-negatives 100 --cross-sample-negatives 0 \
 		--max-sample-size 250000 --min-sample-size 32000 --dropout 0.1 --attention-dropout 0.1 --weight-decay 0.01 \
 		--max-tokens 1400000 --max-update 400000 --skip-invalid-size-inputs-valid-test --ddp-backend no_c10d \
-        --load-extractor --distill-with-ce 2>&1 | tee $expdir/train.log
+        --load-extractor --distill-with-kl 2>&1 | tee $expdir/train.log
 
 
         #--encoder-attention-heads 6 --encoder-embed-dim 480 --encoder-ffn-embed-dim 1920 --encoder-layers 6 --latent-vars 160 \
